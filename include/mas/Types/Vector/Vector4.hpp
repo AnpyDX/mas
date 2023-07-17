9a9b423e97faa7efc753d0c4048c92b661325fe9
anpyd <leidish@foxmail.com>
2023-07-17 14:51:36 +0800

    impl Vector4 operator overload

--- include/mas/Types/Vector/Vector4.hpp
+++ include/mas/Types/Vector/Vector4.hpp
@@ -1,5 +1,5 @@
 /**
- * @project: mas-lib
+ * @project: mas-lib (https://github.com/anpydx/mas)
  * @brief Vector Type 4D
  * @version 0.1
  */
@@ -43,4 +43,50 @@ namespace mas {
             T a;
         };
     };
+
+    template <typename T>
+    inline Vector4<T> operator-(const Vector4<T>& v) {
+        return Vector4<T> { -v.x, -v.y, -v.z, -v.w };
+    }
+
+    template <typename T>
+    inline Vector4<T> operator+(const Vector4<T>& v1, const Vector4<T>& v2) {
+        return Vector4<T> { v1.x + v2.x,  v1.y + v2.y, v1.z + v2.z, v1.w + v2.w };
+    }
+
+    template <typename T>
+    inline Vector4<T> operator-(const Vector4<T>& v1, const Vector4<T>& v2) {
+        return Vector4<T> { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z, v1.w - v2.w };
+    }
+
+    template <typename T>
+    inline Vector4<T> operator*(const Vector4<T>& v1, const Vector4<T>& v2) {
+        return Vector4<T> { v1.x * v2.x, v1.y * v2.y, v1.z * v2.z, v1.w * v2.w };
+    }
+
+    template <typename T>
+    inline Vector4<T> operator/(const Vector4<T>& v1, const Vector4<T>& v2) {
+        return Vector4<T> { v1.x / v2.x, v1.y / v2.y, v1.z / v2.z, v1.w / v2.w };
+    }
+
+    template <typename T1, typename T2>
+    inline Vector4<T1> operator*(const Vector4<T1>& v, const T2& n) {
+        return Vector4<T1> { v.x * static_cast<T1>(n), v.y * static_cast<T1>(n), v.z * static_cast<T1>(n), v.w * static_cast<T1>(n) };
+    }
+
+    template <typename T1, typename T2>
+    inline Vector4<T2> operator*(const T1& n, const Vector4<T2>& v) {
+        return v * n;
+    }
+
+    template <typename T>
+    inline Vector4<T> operator/(const Vector4<T>& v, const T& n) {
+        return Vector4<T> { v.x / static_cast<T>(n), v.y / static_cast<T>(n), v.z / static_cast<T>(n), v.w / static_cast<T>(n) };
+    }
+
+    template <typename T>
+    inline Vector4<T> operator/(const T& n, const Vector4<T>& v) {
+        return Vector4<T> { static_cast<T>(n) / v.x, static_cast<T>(n) / v.y, static_cast<T>(n) / v.z, static_cast<T>(n) / v.w };
+    }
+
 }
