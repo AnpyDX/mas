00a1666c0ba688864c8bc0fd19142892bf090067
anpyd <leidish@foxmail.com>
2023-07-17 15:08:00 +0800

    impl funcs

--- include/mas/Types/Vector/Helper.hpp
+++ include/mas/Types/Vector/Helper.hpp
@@ -21,7 +21,7 @@ namespace mas {
     * @brief cast Vector2<int> to Vector2<dst_T>
     */
     template <typename T>
-    Vector2<T> cast_to(const Vector2<int>& v) {
+    inline Vector2<T> cast_to(const Vector2<int>& v) {
         return Vector2<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
     }
     
@@ -29,7 +29,7 @@ namespace mas {
     * @brief cast Vector2<float> to Vector2<dst_T>
     */
     template <typename T>
-    Vector2<T> cast_to(const Vector2<float>& v) {
+    inline Vector2<T> cast_to(const Vector2<float>& v) {
         return Vector2<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
     }
 
@@ -37,15 +37,73 @@ namespace mas {
     * @brief cast Vector2<double> to Vector2<dst_T>
     */
     template <typename T>
-    Vector2<T> cast_to(const Vector2<double>& v) {
+    inline Vector2<T> cast_to(const Vector2<double>& v) {
         return Vector2<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
     }
-    
+
+    /**
+    * @brief cast Vector3<int> to Vector3<dst_T>
+    */
+    template <typename T>
+    inline Vector3<T> cast_to(const Vector3<int>& v) {
+        return Vector3<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
+    /**
+    * @brief cast Vector3<float> to Vector3<dst_T>
+    */
+    template <typename T>
+    inline Vector3<T> cast_to(const Vector3<float>& v) {
+        return Vector3<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
+    /**
+    * @brief cast Vector3<double> to Vector3<dst_T>
+    */
+    template <typename T>
+    inline Vector3<T> cast_to(const Vector3<double>& v) {
+        return Vector3<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
+    /**
+    * @brief cast Vector4<int> to Vector4<dst_T>
+    */
+    template <typename T>
+    inline Vector4<T> cast_to(const Vector4<int>& v) {
+        return Vector4<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
+    /**
+    * @brief cast Vector4<float> to Vector4<dst_T>
+    */
+    template <typename T>
+    inline Vector4<T> cast_to(const Vector4<float>& v) {
+        return Vector4<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
+    /**
+    * @brief cast Vector4<double> to Vector4<dst_T>
+    */
+    template <typename T>
+    inline Vector4<T> cast_to(const Vector4<double>& v) {
+        return Vector4<T> { static_cast<T>(v.x), static_cast<T>(v.y) };
+    }
+
 
     /** @section Get Vector Length */
     template <typename T>
-    inline T length(Vector2<T> v) {
+    inline double length(Vector2<T> v) {
         return std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2));
+    }
+
+    template <typename T>
+    inline double length(Vector3<T> v) {
+        return std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2) + std::pow(v.z, 2));
+    }
+
+    template <typename T>
+    inline double length(Vector4<T> v) {
+        return std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2) + std::pow(v.z, 2) + std::pow(v.w, 2));
     }
 
     /** @section Get Normalized Vector */
@@ -57,7 +115,20 @@ namespace mas {
 
     /** @section Dot Product of Vector */
     template <typename T>
-    T dot(const Vector2<T>& v1, const Vector2<T>& v2) {
+    inline T dot(const Vector2<T>& v1, const Vector2<T>& v2) {
         return v1.x * v2.x + v1.y * v2.y;
     }
+
+    template <typename T>
+    inline T dot(const Vector3<T>& v1, const Vector3<T>& v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    }
+
+    template <typename T>
+    inline T dot(const Vector4<T>& v1, const Vector4<T>& v2) {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z +  v1.w * v2.w;
+    }
+
+    /** @section Cross Product of Vector */
+
 }
