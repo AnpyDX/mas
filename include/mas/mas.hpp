ca586caa5b95b70a86fb46280744aacf15ee8387
anpyd <leidish@foxmail.com>
2023-07-17 13:46:08 +0800

    add Lite Type marco

--- include/mas/mas.hpp
+++ include/mas/mas.hpp
@@ -9,7 +9,30 @@
 
 #pragma once
 
+/** @section Vector Types */
 #include "Types/Vector/Vector2.hpp"
 #include "Types/Vector/Vector3.hpp"
 #include "Types/Vector/Vector4.hpp"
 #include "Types/Vector/Helper.hpp"
+
+
+#define MAS_LTYPE_FLOAT
+/* lite types marco */
+namespace mas {
+#ifdef MAS_LTYPE_INT
+    using vec2 = Vector2<int>;
+    using vec3 = Vector3<int>;
+    using vec4 = Vector4<int>;
+
+#elifdef MAS_LTYPE_FLOAT
+    using vec2 = Vector2<float>;
+    using vec3 = Vector3<float>;
+    using vec4 = Vector4<float>;
+
+#elifdef MAS_LTYPE_DOUBLE
+    using vec2 = Vector2<double>;
+    using vec3 = Vector3<double>;
+    using vec4 = Vector4<double>;
+
+#endif
+}
